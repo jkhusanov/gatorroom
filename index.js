1516185a81450664027852972e208c7b4c0c206e
Jkhusanov <khusanov.jakhongir@gmail.com>
2019-07-14 18:00:40 -0700

    Set the logic of running code on production

--- index.js
+++ index.js
@@ -192,12 +192,12 @@ app.route('/api/upload').post((req, res) => {
 /**
  * Runs code in production mode
  */
-// if (process.env.NODE_ENV === 'production') {
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
