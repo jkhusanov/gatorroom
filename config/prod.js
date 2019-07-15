ddee2a95e8c7900b96a035a0b4af403d922d6e59
Jkhusanov <khusanov.jakhongir@gmail.com>
2019-07-14 17:07:12 -0700

    Hide prodection keys in server

--- config/prod.js
+++ config/prod.js
@@ -1,12 +1,12 @@
 // prod.js - production keys in here!!!
 
 module.exports = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'password',
-  database: 'gatorroom',
-  googleClientID: '939388377545-ic81cgmlveutqd497tkklunnj27c3svh.apps.googleusercontent.com',
-  googleClientSecret: 'Y5h3kSi05h-RmkjpKag6UbvR',
-  cookieKey: 'sdfknjDSFjknj3k4ndsii3invjccmdmnfjqklnjkfeqwffedscvnxjj4k3FD-4j-4dfnsdafmnlj2jknsdj',
-  googleLatLong: 'AIzaSyCKSxjOsN5bYFi9QtYqbrLoPtknm8yr5E0',
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  cookieKey: process.env.COOKIE_KEY,
+  googleLatLong: process.env.GOOGLE_MAPS,
 };
